ec37212215628eb7a008b55759cee23505c45f39
jaydeep <jaydeepsathwara272@gmail.com>
2022-11-20 22:56:30 +0530

    Initial commit

---
+++ app/src/main/java/com/jaydeepsathwara/practical_10_20012011154/user_view_design.kt
@@ -0,0 +1,11 @@
+package com.jaydeepsathwara.practical_10_20012011154
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user_view_design : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_view_design)
+    }
+}
